e88af11ea2d2e39e67bfbcf46c93e4f7a0204a70
Rolando E. Sotomayor <resotomayor85@gmail.com>
2017-05-15 11:24:20 -0400

    Restoting datasource

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,66 +1,78 @@
-   var arr = [-7 ,1, 5, 31, 54, 87, 99, 102, 205, 254, 268, 309, 312, 378, 588, 590, 750, 1000, 4535, 7523];
+var arr = [-7 ,1, 5, 31, 54, 87, 99, 102, 205, 254, 268, 309, 312, 378, 588, 590, 750, 1000, 4535, 7523];
 
-   var data = document.getElementById("data").innerHTML;
-   document.getElementById("data").innerHTML = (data + "[ " + arr + " ]").replace(/,/g, ", ");
 
-   var errorMsg = document.getElementById("errorMsg");
+var data = document.getElementById("data").innerHTML;
+document.getElementById("data").innerHTML = (data + "[ " + arr + " ]").replace(/,/g, ", ");
 
-   var result = document.getElementById("result");
+var errorMsg = document.getElementById("errorMsg");
 
-   var prevKeysFound = [];
-   var prevKeysNotFound = [];
+var result = document.getElementById("result");
 
-   function rank(k, a) {
-       if (a instanceof Array) {
-           var lo = 0;
-           var hi = a.length - 1;
-           var mid;
-           while (lo <= hi) {
-            mid = Math.floor(lo + (hi - lo) / 2);
-               if (k < a[mid])
-                   hi = mid - 1;
-               else if (k > a[mid])
-                   lo = mid + 1;
-               else
-                   return mid;
-           }
-           return -1;
-       }
-   }
+var prevKeysFound = [];
+var prevKeysNotFound = [];
 
-   // This function executes when click event is fired by button
-   function display() {
+function rank(k, a) {
+  if (a instanceof Array) {
+    var lo = 0;
+    var hi = a.length - 1;
+    var mid;
+    while (lo <= hi) {
+      mid = Math.floor(lo + (hi - lo) / 2);
+      if (k < a[mid]) hi = mid - 1;
+      else if (k > a[mid]) lo = mid + 1;
+      else return mid;
+    }
+    return -1;
+  }
+}
 
-       var val = document.getElementById("keyValue").value;
-       if (val === '') {
-           errorMsg.style.display = "block";
-       } else {
-           errorMsg.style.display = "none";
+// This function executes when click event is fired by button
+function display() {
+  var val = document.getElementById("keyValue").value;
+  if (val === "") {
+    errorMsg.style.display = "block";
+  } else {
+    errorMsg.style.display = "none";
 
-           var res;
-           var input = rank(val, arr);
+    var res;
+    var input = rank(val, arr);
 
-           if (input !== -1) {
-               var collection = document.getElementById("data").innerHTML;
-               collection = (data + "[ " + arr + " ]").replace(/,/g, ", ");
-               collection = collection.replace(val, "<span class='badge'>" + val + "</span>");
-               /* < sub > " + input + " < /sub>"*/
-               document.getElementById("data").innerHTML = collection;
+    if (input !== -1) {
+      var collection = document.getElementById("data").innerHTML;
+      collection = (data + "[ " + arr + " ]").replace(/,/g, ", ");
+      collection = collection.replace(
+        val,
+        "<span class='badge'>" + val + "</span>"
+      );
+      /* < sub > " + input + " < /sub>"*/
+      document.getElementById("data").innerHTML = collection;
 
-               res = "GREAT, The key <b>" + val + "</b> is located at position <b>" + rank(val, arr) + "</b>.";
-               result.className = "alert alert-success"; //found
+      res =
+        "GREAT, The key <b>" +
+        val +
+        "</b> is located at position <b>" +
+        rank(val, arr) +
+        "</b>.";
+      result.className = "alert alert-success"; //found
 
-               prevKeysFound.push(val);
-               document.getElementById("prevKeysFound").innerHTML = ("[ " + prevKeysFound + " ]").replace(/,/g, ", ");
-           } else {
-               document.getElementById("data").innerHTML = (data + "[ " + arr + " ]").replace(/,/g, ", ");
-               res = "SORRY. The key <b>" + val + "</b> wasn't found in the collection.";
-               result.className = "alert alert-danger"; //notFound
+      prevKeysFound.push(val);
+      document.getElementById("prevKeysFound").innerHTML = ("[ " +
+        prevKeysFound +
+        " ]").replace(/,/g, ", ");
+    } else {
+      document.getElementById("data").innerHTML = (data +
+        "[ " +
+        arr +
+        " ]").replace(/,/g, ", ");
+      res = "SORRY. The key <b>" + val + "</b> wasn't found in the collection.";
+      result.className = "alert alert-danger"; //notFound
 
-               prevKeysNotFound.push(val);
-               document.getElementById("prevKeysNotFound").innerHTML = ("[ " + prevKeysNotFound + " ]").replace(/,/g, ", ");
-           }
-           result.innerHTML = res;
-       }
-       document.getElementById("keyValue").value = "";
-   }
+      prevKeysNotFound.push(val);
+      document.getElementById("prevKeysNotFound").innerHTML = ("[ " +
+        prevKeysNotFound +
+        " ]").replace(/,/g, ", ");
+    }
+    result.innerHTML = res;
+  }
+  document.getElementById("keyValue").value = "";
+}
